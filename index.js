387d0b2eadf348e10c4e541f3502f6d53b4bb9d1
Kevin Chaney <kevin_chaney@icloud.com>
2022-05-13 20:15:20 -0700

    commit tree 98aab726175d0ae0ec140dfc503f59597957b239

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    if(record.find(dub => dub.result === "W")){
+        let win = record.find(dub => dub.result === "W")
+        return win.year
+    }
+    
+}
